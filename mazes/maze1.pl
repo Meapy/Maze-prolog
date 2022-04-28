0359071f4e65928172d1b0aeb7b552b403958ed3
Meapy <dkrasovskijj@gmail.com>
2022-04-28 17:06:26 +0100

    dfs completed

---
+++ mazes/maze1.pl
@@ -0,0 +1,12 @@
+%maze #1
+wall(2,2).
+wall(2,3).
+wall(2,4).
+wall(3,4).
+wall(4,4).
+wall(4,3).
+
+mazeSize(5,5).
+
+start(maze,pos(5,1)).
+goal(maze,pos(1,5)).
---
+++ mazes/maze2.pl
@@ -0,0 +1,23 @@
+%maze #2
+wall(2,4).
+wall(3,4).
+wall(4,4).
+wall(5,4).
+wall(6,4).
+wall(7,4).
+wall(8,4).
+wall(9,4).
+wall(10,4).
+wall(6,5).
+wall(6,6).
+wall(6,7).
+wall(6,8).
+wall(6,9).
+wall(7,7).
+wall(8,7).
+wall(9,7).
+
+mazeSize(10,10).
+
+start(maze,pos(1,1)).
+goal(maze,pos(8,6)).
